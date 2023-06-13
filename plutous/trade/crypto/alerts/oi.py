c7fa25d81973b712c8743fe02f8990e5bc29d185
cheunhong <chlin6755@gmail.com>
2023-06-13 13:28:40 +0000

    update OIAlert

--- plutous/trade/crypto/alerts/oi.py
+++ plutous/trade/crypto/alerts/oi.py
@@ -13,14 +13,25 @@ class OIAlert(BaseAlert):
     config: OIAlertConfig
 
     def run(self):
+        if self.config.lookback < 2:
+            raise ValueError("Lookback cannot be less than 2")
+
         df = self.data[OpenInterest.__tablename__]
-        df = df.pct_change().iloc[-1]
-        df = df[df>self.config.threshold]
+        df = df.iloc[-1] / df.iloc[0] - 1
+        df = df[df > self.config.threshold]
         if df.empty:
             return
-        
-        frequency = self.config.frequency.replace('m', 'min').replace('h', 'hr')
-        msg = f"**Open Interest Alert ({frequency})** \n"
+
+        interval, frequnecy = (
+            int(self.config.frequency[:-1]) * self.config.lookback - 1,
+            self.config.frequency[-1].replace("m", "min").replace("h", "hr"),
+        )
+        if (interval >= 60) & (frequnecy == "min"):
+            if interval % 60 == 0:
+                frequnecy = "hr"
+                interval = interval // 60
+
+        msg = f"**OI Variation Alert (last {interval}{frequnecy})** \n"
         for symbol, pct in df.items():
             msg += f"{symbol.split(':')[0]}: {pct:.2%} \n"
 
